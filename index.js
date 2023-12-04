005964667f90a6b0fad7d69c0a7032fea2f68bbb
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-12-04 15:22:34 -0500

    express and localhost3000 initialization

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 //To see how the final website should work, run "node solution.js".
 //Make sure you have installed all the dependencies with "npm i".
 //The password is ILoveProgramming
+
+import express from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+const port = 3000;
+
+
+app.get("/", (req, res)=> {
+    
+    res.send("Hello!")
+     
+})
+
+
+
+app.listen(port, () => {
+    
+    console.log(`Listening on port ${port}`);
+     
+})

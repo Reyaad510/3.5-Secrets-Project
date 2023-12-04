a405867df62920a38aea6d1b8d8cb215fd72fd16
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-12-04 15:34:40 -0500

    POST functioning without correct password

--- index.js
+++ index.js
@@ -14,10 +14,20 @@ const app = express();
 const port = 3000;
 
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get("/", (req, res)=> {
     
     res.sendFile(__dirname + "/public/index.html");
      
+})
+
+
+app.post("/check", (req,res)=> {
+
+    res.sendFile(__dirname + "/public/secret.html");
+
 })
 
 

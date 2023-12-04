5255c6cd8ef4f72ae17daf35a6a10b16f775bc81
Nicholas Reyaad Rafeek <45085276+Reyaad510@users.noreply.github.com>
2023-12-04 15:30:31 -0500

    File path corrected for app.get

--- index.js
+++ index.js
@@ -5,13 +5,18 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
 const app = express();
 const port = 3000;
 
 
 app.get("/", (req, res)=> {
     
-    res.send("Hello!")
+    res.sendFile(__dirname + "/public/index.html");
      
 })
 
